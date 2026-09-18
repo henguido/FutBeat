8e1fb2262672b2452158a15155763c010f60d89b
henguido <hen_guido@hotmail.com>
2026-09-18 13:19:37 -0700

    test(match): satisfy detail repository contract

--- apps/mobile/test/flow_test.dart
+++ apps/mobile/test/flow_test.dart
@@ -40,6 +40,9 @@ class TestRepository implements FootballRepository {
 
   @override
   Future<Snapshot> loadDate(DateTime date) => load();
+
+  @override
+  Future<MatchDetail> loadMatchDetail(String id) async => MatchDetail.empty(id);
 }
 
 Future<void> openApp(
